358814cd3d762a9f8d25a11e2f11effcc2b366c1
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2024-02-03 00:01:07 +0500

    Update .env and .gitignore, add login view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Board from "@/components/Board";
+import LoginView from "@/components/views/LoginView";
 import { authOptions } from "@/lib/authOptions";
 import { getServerSession } from "next-auth";
 
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
+  if (!session){
+    return (<div>
+      <LoginView />
+    </div>)
+  }
   return (
     <div>
       <Board />
