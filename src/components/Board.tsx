73a75e20d5569ae06eb15451ac0b2cec6ec7696b
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2024-02-04 00:03:08 +0500

    Update board, column components with rename functionality

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,14 +1,40 @@
 "use client";
-import React from "react";
-import { RoomProvider } from "@/app/liveblocks.config";
+import React, { FormEvent, useEffect, useState } from "react";
+import { RoomProvider, useUpdateMyPresence } from "@/app/liveblocks.config";
 import { LiveList } from "@liveblocks/core";
 import { ClientSideSuspense } from "@liveblocks/react";
 import { Columns } from "./Columns";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
+import { useRouter } from "next/navigation";
+import { updateBoard } from "@/app/actions/boardActions";
 
 const Board = ({ id, name }: { id: string; name: string }) => {
+  const [renameMode, setRenameMode] = useState(false);
+  const router = useRouter();
+  // const updateMyPresence = useUpdateMyPresence();
+
+  // useEffect(() => {
+  //   updateMyPresence({ boardId: id });
+
+  //   return () => {
+  //     updateMyPresence({ boardId: null });
+  //   };
+  // }, []);
+
+  async function handleNameSubmit(ev: FormEvent) {
+    ev.preventDefault();
+    const input = (ev.target as HTMLFormElement).querySelector("input");
+    if (input) {
+      const newName = input.value;
+      await updateBoard(id, { metadata: { boardName: newName } });
+      input.value = "";
+      setRenameMode(false);
+      router.refresh();
+    }
+  }
+
   return (
     <RoomProvider
       id={id}
@@ -26,7 +52,16 @@ const Board = ({ id, name }: { id: string; name: string }) => {
           <>
             <div className="flex gap-2 justify-between items-center mb-4">
               <div>
-                <h1 className="text-2xl">Board: {name}</h1>
+                {!renameMode && (
+                  <h1 className="text-2xl" onClick={() => setRenameMode(true)}>
+                    Board: {name}
+                  </h1>
+                )}
+                {renameMode && (
+                  <form onSubmit={handleNameSubmit}>
+                    <input type="text" defaultValue={name} />
+                  </form>
+                )}
               </div>
               <Link
                 className="flex gap-2 items-center btn"
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import { ReactSortable } from "react-sortablejs";
 import { Card, useMutation, useStorage } from "@/app/liveblocks.config";
 import NewCardForm from "./forms/NewCardForm";
 import { shallow } from "@liveblocks/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsis, faTrash } from "@fortawesome/free-solid-svg-icons";
+import CancelButton from "./CancelButton";
 
 type ColumnProps = {
   id: string;
@@ -10,6 +13,9 @@ type ColumnProps = {
 };
 
 export default function Column({ id, name }: ColumnProps) {
+
+  const [renameMode, setRenameMode] = useState(false);
+
   const columnCards = useStorage<Card[]>((root) => {
     return root.cards
       .filter((card) => card.columnId === id)
@@ -25,6 +31,17 @@ export default function Column({ id, name }: ColumnProps) {
       }
     }
   }, []);
+
+  const updateColumn = useMutation(({storage}, id, newName) => {
+    const columns = storage.get('columns');
+    columns.find(c => c.toObject().id === id)?.set('name', newName);
+  }, []);
+
+  const deleteColumn = useMutation(({storage}, id) => {
+    const columns = storage.get('columns');
+    const columnIndex = columns.findIndex(c => c.toObject().id === id);
+    columns.delete(columnIndex);
+  }, [])
 
   const setTasksOrderForColumn = useMutation(
     ({ storage }, sortedCards: Card[], newColumnId) => {
@@ -45,31 +62,60 @@ export default function Column({ id, name }: ColumnProps) {
     []
   );
 
+  function handleRenameSubmit(ev: FormEvent) {
+    ev.preventDefault();
+    const input = (ev.target as HTMLFormElement).querySelector('input');
+    if (input) {
+      const newColumnName = input.value;
+      updateColumn(id, newColumnName);
+      setRenameMode(false);
+    }
+  }
+
   return (
     <div className="w-48 bg-white shadow-sm rounded-md p-2">
-      <h3>{name}</h3>
-      {columnCards && (
+      {!renameMode && (
+        <div className="flex justify-between">
+          <h3>{name}</h3>
+          <button className="text-gray-300" onClick={() => setRenameMode(true)}>
+            <FontAwesomeIcon icon={faEllipsis} />
+          </button>
+        </div>
+      )}
+      {renameMode && (
+        <div className="mb-8">
+          Edit name:
+          <form onSubmit={handleRenameSubmit} className="mb-2">
+            <input type="text" defaultValue={name}/>
+            <button type="submit" className="w-full mt-2">Save</button>
+          </form>
+          <button
+            onClick={() => deleteColumn(id)}
+            className="bg-red-500 text-white p-2 flex gap-2 w-full items-center rounded-md justify-center">
+            <FontAwesomeIcon icon={faTrash} />
+            Delete column
+          </button>
+          <CancelButton onClick={() => setRenameMode(false)} />
+        </div>
+      )}
+      {!renameMode && columnCards && (
         <>
           <ReactSortable
             list={columnCards}
-            setList={(items) => setTasksOrderForColumn(items, id)}
+            setList={items => setTasksOrderForColumn(items, id)}
             group="cards"
             className="min-h-12"
             ghostClass="opacity-40"
           >
-            {columnCards.map((card) => (
-              <div
-                className="border bg-white my-2 p-4 rounded-md"
-                key={card.id}
-              >
-                <span>{card.name}</span>
-              </div>
-            ))}
+            {/* {columnCards.map(card => (
+              <ColumnCard key={card.id} id={card.id} name={card.name} />
+            ))} */}
           </ReactSortable>
         </>
       )}
-
-      <NewCardForm columnId={id} />
+      {!renameMode && (
+        <NewCardForm columnId={id} />
+      )}
     </div>
   );
 }
